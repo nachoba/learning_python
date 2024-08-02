49918624b6adea947fac1fe71cca48748db453da
nachoba <0800nacho@gmail.com>
2024-08-01 21:55:47 -0300

    iterators

---
+++ IGP3/003-iteration.py
@@ -0,0 +1,10 @@
+# Iteration
+# =========
+# A common idiom when dealing with sequences is to loop over the contents of
+# the sequence.
+# The 'for' loop is one way to do this:
+
+letters = ['c', 'a', 't', 's']
+
+for letter in letters:
+    print(letter)
